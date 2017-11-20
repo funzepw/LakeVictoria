d58d241bec61d8642e6d40da4622fa1762414ca9
funzepw <31410560+funzepw@users.noreply.github.com>
2017-11-20 16:41:18 -0500

    Add files via upload

---
+++ research_institutions.js
@@ -0,0 +1,10 @@
+var institutions ={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "Id": 0, "Institutio": "National Fisheries Resources Research Institute ,Jinja, Uganda(NaFIRRI)" }, "geometry": { "type": "Point", "coordinates": [ 33.2071014, 0.4168076 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Institutio": "Tanzania Fisheries Research Institute, Mwanza, Tanzania (TAFIRI)" }, "geometry": { "type": "Point", "coordinates": [ 32.8891, -2.561383 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Institutio": "Kenya Marine and Fisheries Reserach Institute, Kisumu, Kenya (KMFRI)" }, "geometry": { "type": "Point", "coordinates": [ 34.737710, -0.088413 ] } }
+]
+};
